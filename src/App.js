ee1fbdb1ee2034b54af9265654a6e99e20b67453
yadira <yadiraco304@gmail.com>
2020-09-01 19:19:06 -0500

    Improve code

--- src/App.js
+++ src/App.js
@@ -1,57 +1,37 @@
 import React from "react";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
-import CancelIcon from "@material-ui/icons/Cancel";
 import "./Task.css";
 
-const App = ({ todo, handleDelete }) => {
+const App = ({ todoList, handleDelete }) => {
   return (
     <>
-      {todo.iu.length === 0 ? (
+      {todoList.length === 0 ? (
         <p className="cantidad">Add a task</p>
       ) : (
-        <p className="cantidad">You have {todo.iu.length} taks to do</p>
+        <p className="cantidad">You have {todoList.length} taks to do</p>
       )}
 
-      {todo.iu.map((task, id) => (
-        <ul key={id} id="li">
-          {task.length === 0 ? null : (
-            <li className="contenedor" id="li_li">
-              <div className="div">
-                <CheckCircleIcon
-                  style={{
-                    fontSize: 30,
-                    float: "left",
-                    color: "rgb(86, 86, 243)",
-                  }}
-                  onClick={() => {
-                    {
-                      task.color === "blue"
-                        ? console.log("d")
-                        : console.log("w");
-                    }
-                    // return console.log()
-                  }}
-                />
-
-                <CancelIcon
-                  style={{ fontSize: 30, color: "#f05252", float: "left" }}
-                  onClick={() => {
-                    let conjunto = document.getElementById("li_li");
-                    conjunto.style.color = "red";
-                    return conjunto;
-                  }}
-                />
-                {task}
-                <DeleteOutlineIcon
-                  style={{ float: "right" }}
-                  onClick={() => handleDelete(id)}
-                />
-              </div>
-            </li>
-          )}
-        </ul>
-      ))}
+      <ul>
+        {todoList.map(({ id, text }) => (
+          <li key={id} className="contenedor">
+            <div className="div">
+              <CheckCircleIcon
+                style={{
+                  fontSize: 30,
+                  float: "left",
+                  color: "rgb(86, 86, 243)",
+                }}
+              />
+              {text}
+              <DeleteOutlineIcon
+                style={{ float: "right" }}
+                onClick={() => handleDelete(id)}
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
